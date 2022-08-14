93e7ec61cf20de0bd07f5a6288e399e9f7e397f8
joseluisrayo <jcarihuasairorayo@gmail.com>
2022-08-14 18:19:43 -0500

    update cache add redis.exe

--- src/routers/cache.js
+++ src/routers/cache.js
@@ -1,14 +1,20 @@
 import ExpressExpeditious from "express-expeditious";
+import expeditiousEngineRedis from "expeditious-engine-redis";
 
 const defaultOptions = ({
     namespace: 'expresscache',
-    defaultTtl: '1 minute',
+    // defaultTtl: '1 minute',
+    defaultTtl: 60 * 1000,
     statusCodeExpires: {
         404: '5 minutes',
         500: 0 // 1 minute in miliseconds
-    }
+    },
+    engine: expeditiousEngineRedis({
+        host: '127.0.0.1',
+        port: 6379
+    })
 })
 
 const cacheInit = ExpressExpeditious(defaultOptions);
 
-module.exports = {cacheInit}
+module.exports = { cacheInit };
