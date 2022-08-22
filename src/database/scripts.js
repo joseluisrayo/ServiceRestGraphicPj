7cdfa916ebce265499eb347ce398f3e7aa81dd10
joseluisrayo <jcarihuasairo@pj.gob.pe>
2022-08-22 14:27:20 -0500

    recorrecion scripts

--- src/database/scripts.js
+++ src/database/scripts.js
@@ -162,92 +162,79 @@ const ListadoProgramaciones = (fecha) => `
 const ListadoProgramacionesFirmadoPonente = (n_sala, fecha) => `
     SELECT
     RTRIM(z.c_usuario_vocal) AS "00_Ponente" ,
-    count(CASE WHEN z.estadop + z.estadom >= 1 OR z.num_tipo_audiencia = 5 THEN 1  END) AS "02_Resuelto",
-    count(CASE WHEN  z.estadop + z.estadom = 0 AND z.num_tipo_audiencia <> 5 THEN 1  END) AS "04_Pendiente", 
-    z.anno AS '03_anno', 
-    z.mes  as '01_mes' 
+        count(CASE WHEN z.mes = 1  THEN 1 END) AS "01_Enero",
+        count(CASE WHEN z.mes = 2  THEN 1 END) AS "02_Febrero",
+        count(CASE WHEN z.mes = 3  THEN 1 END) AS "03_Marzo",
+        count(CASE WHEN z.mes = 4  THEN 1 END) AS "04_Abril",
+        count(CASE WHEN z.mes = 5  THEN 1 END) AS "05_Mayo",
+        count(CASE WHEN z.mes = 6  THEN 1 END) AS "06_Junio",
+        count(CASE WHEN z.mes = 7  THEN 1 END) AS "07_Julio",
+        count(CASE WHEN z.mes = 8  THEN 1 END) AS "08_Agosto",    
+        count(CASE WHEN z.mes = 9  THEN 1 END) AS "09_Setiembre",
+        count(CASE WHEN z.mes = 10 THEN 1 END) AS "10_Octubre",
+        count(CASE WHEN z.mes = 11 THEN 1 END) AS "11_Noviembre",
+        count(CASE WHEN z.mes = 12 THEN 1 END) AS "12_Diciembre"  
     FROM (
-            SELECT  
-                y.c_usuario_vocal,
-                y.anno ,
-                y.mes ,
-                y.num_tipo_audiencia ,
-                (SELECT count(1)
-                                FROM ResolucionEditorFirma b
-                            WHERE b.n_unico = y.n_unico AND
-                                        b.n_incidente = y.n_incidente AND
-                                        b.c_usuario    = y.c_usuario_vocal AND
-                                        b.l_firmado = 'S' AND
-                                        b.f_firma >= convert(DATE, y.f_programacion) AND 
-                                        b.l_indPonente = 'S' AND b.l_activo = 'S' AND
-                                        b.f_firma = ( SELECT max(x.f_firma) 
-                                                                FROM ResolucionEditorFirma x
-                                                                JOIN resolucion_editor r ON 
-                                                                        r.n_unico = x.n_unico AND
-                                                                        r.n_incidente = x.n_incidente AND
-                                                                        r.f_descargo = x.f_descargo AND
-                                                                        IsNull(r.l_utilizado, 'N') <> 'A' AND
-                                                                        IsNull(r.l_ind_sumilla, 'N') <> 'N'
-                                                                    WHERE x.n_unico = b.n_unico AND
-                                                                        x.n_incidente = b.n_incidente AND
-                                                                        x.c_usuario = b.c_usuario AND
-                                                                        x.f_firma >= convert(DATE, y.f_programacion)  AND 
-                                                                        x.l_firmado = 'S' AND
-                                                                        x.l_indPonente = 'S' AND x.l_activo = 'S' )  ) AS estadop ,
-                            (SELECT count(1)
-                                FROM ResolucionEditorMovimFirma b
-                            WHERE b.n_unico       = y.n_unico AND
-                                        b.n_incidente = y.n_incidente AND
-                                        b.c_usuario    = y.c_usuario_vocal AND
-                                        b.l_firmado = 'S' AND
-                                        b.f_firma >= convert(DATE, y.f_programacion) AND 
-                                        b.l_indPonente = 'S' AND b.l_activo = 'S' AND
-                                        b.f_firma = ( SELECT max(x.f_firma) 
-                                                                FROM ResolucionEditorMovimFirma x
-                                                                JOIN resolucion_editor r ON 
-                                                                        r.n_unico = x.n_unico AND
-                                                                        r.n_incidente = x.n_incidente AND
-                                                                        r.f_descargo = x.f_descargo AND
-                                                                        IsNull(r.l_utilizado, 'N') <> 'A' AND
-                                                                        IsNull(r.l_ind_sumilla, 'N') <> 'N'
-                                                                    WHERE x.n_unico = b.n_unico AND
-                                                                        x.n_incidente = b.n_incidente AND
-                                                                        x.c_usuario = b.c_usuario AND
-                                                                        x.f_firma >= convert(DATE, y.f_programacion)  AND 
-                                                                        x.l_firmado = 'S' AND
-                                                                        x.l_indPonente = 'S' AND x.l_activo = 'S' ) 	   ) AS estadom
-            FROM
-            ( SELECT      
-                cg.n_unico ,
-                cg.n_incidente ,
-                cg.c_usuario_vocal      ,
-                cg.f_programacion ,
-                year(cg.f_programacion) anno,
-                m.x_descripcion mes,
-                cg.num_tipo_audiencia 
-                FROM instancia_expediente ie (INDEX insta_expe_unic)  noholdlock
-                        JOIN expediente e noholdlock
-                        ON e.n_unico=ie.n_unico
-                        AND e.n_incidente=ie.n_incidente
-                        JOIN conformacion_grupo   cg noholdlock 
-                        ON ie.c_distrito = cg.c_distrito AND ie.c_provincia = cg.c_provincia 
-                            AND ie.c_instancia = cg.c_instancia AND ie.n_unico = cg.n_unico 
-                            AND ie.n_incidente = cg.n_incidente AND ie.f_ingreso = cg.f_ingreso 
-                            AND cg.l_ultimo = 'S' AND cg.l_ultimo_audiencia = 'S' 
-                            AND cg.l_reprogramado = 'N' AND cg.l_no_vista = 'N' AND cg.l_publicado = 'S'
-                            AND cg.c_usuario_vocal IS NOT NULL
-                        JOIN meses m
-                        ON m.n_mes = month(cg.f_programacion)      
-                WHERE 	ie.c_distrito = '50'
-                        AND  ie.c_provincia = '01'
-                        AND   ie.c_instancia = '${n_sala}'   
-                        and   cg.f_programacion BETWEEN ${fecha}   
-                        AND ie.l_ultimo = 'S'
-                        AND ISNULL(e.l_anulado,'N') = 'N'  ) y  )  z
-    GROUP BY  z.c_usuario_vocal , 
-                        z.anno ,
-                        z.mes
-    ORDER BY 1 
+    SELECT  
+    y.c_usuario_vocal,  
+    y.n_unico ,  y.n_incidente ,
+    y.n_num_recurso, y.n_ano_recurso, y.nom_recurso ,y.f_programacion ,
+    y.anno ,
+    y.mes ,
+    (SELECT convert(DATE, b.f_firma)  
+    FROM ResolucionEditorFirma b
+    WHERE b.n_unico = y.n_unico AND
+    b.n_incidente = y.n_incidente AND
+    b.c_usuario    = y.c_usuario_vocal AND
+    b.l_firmado = 'S' AND
+    b.l_indPonente = 'S' AND b.l_activo = 'S' AND
+                b.f_firma = ( SELECT max(x.f_firma)
+        FROM ResolucionEditorFirma x
+        JOIN resolucion_editor r ON
+        r.n_unico = x.n_unico AND
+    r.n_incidente = x.n_incidente AND
+    r.f_descargo = x.f_descargo AND
+    IsNull(r.l_utilizado, 'N') <> 'A' AND
+    IsNull(r.l_ind_sumilla, 'N') <> 'N'
+    WHERE x.n_unico = b.n_unico AND
+    x.n_incidente = b.n_incidente AND
+    x.c_usuario = b.c_usuario AND
+    convert(DATE, x.f_firma)  >= convert(DATE, y.f_programacion)  AND
+    x.l_firmado = 'S' AND
+    x.l_indPonente = 'S' AND x.l_activo = 'S' )  ) AS firma    
+    FROM
+    ( SELECT      
+    cg.n_unico ,
+    cg.n_incidente ,
+    cg.c_usuario_vocal      ,  
+    ie.n_exp_sala n_num_recurso,
+        ie.n_ano_sala n_ano_recurso ,
+        mim.x_desc_motivo_ingreso nom_recurso ,
+    cg.f_programacion ,
+    year(cg.f_programacion) anno,
+    month(cg.f_programacion)  mes,
+    cg.num_tipo_audiencia
+    FROM instancia_expediente ie (INDEX insta_expe_unic)  noholdlock
+    JOIN expediente e noholdlock
+    ON e.n_unico=ie.n_unico
+    AND e.n_incidente=ie.n_incidente
+    JOIN conformacion_grupo   cg noholdlock
+        ON ie.c_distrito = cg.c_distrito AND ie.c_provincia = cg.c_provincia
+                AND ie.c_instancia = cg.c_instancia AND ie.n_unico = cg.n_unico
+                AND ie.n_incidente = cg.n_incidente AND ie.f_ingreso = cg.f_ingreso
+                AND cg.l_ultimo = 'S' AND cg.l_ultimo_audiencia = 'S'
+                AND cg.l_reprogramado = 'N' AND cg.l_no_vista = 'N' AND cg.l_publicado = 'S'
+                AND cg.c_usuario_vocal IS NOT NULL    
+        JOIN motivo_ingreso_maestro mim noholdlock ON mim.c_motivo_ingreso = ie.c_motivo_ingreso      
+    WHERE ie.c_distrito = '50'
+    AND ie.c_provincia = '01'
+    AND ie.c_instancia = '${n_sala}'    
+    AND cg.f_programacion BETWEEN ${fecha}  
+    AND ie.l_ultimo = 'S'
+    AND cg.num_tipo_audiencia <> 5
+    AND ISNULL(e.l_anulado,'N') = 'N'  ) y     )  z
+    WHERE z.f_programacion  <  isnull(z.firma , z.f_programacion)  
+    GROUP BY  z.c_usuario_vocal  
 `;
 
 //QUERY ESCRITOS
@@ -316,77 +303,42 @@ const ListaTipoEscritos = (n_sala, fecha) => `
 `;
 
 const ListadoEscritosPendienteAtendido = (n_sala, fecha) => `
-    SELECT
-    RTRIM(z.c_usuario_vocal) AS "00_Ponente" ,
-        count(CASE WHEN z.mes = 1  THEN 1 END) AS "01_Enero",
-        count(CASE WHEN z.mes = 2  THEN 1 END) AS "02_Febrero",
-        count(CASE WHEN z.mes = 3  THEN 1 END) AS "03_Marzo",
-        count(CASE WHEN z.mes = 4  THEN 1 END) AS "04_Abril",
-        count(CASE WHEN z.mes = 5  THEN 1 END) AS "05_Mayo",
-        count(CASE WHEN z.mes = 6  THEN 1 END) AS "06_Junio",
-        count(CASE WHEN z.mes = 7  THEN 1 END) AS "07_Julio",
-        count(CASE WHEN z.mes = 8  THEN 1 END) AS "08_Agosto",    
-        count(CASE WHEN z.mes = 9  THEN 1 END) AS "09_Setiembre",
-        count(CASE WHEN z.mes = 10 THEN 1 END) AS "10_Octubre",
-        count(CASE WHEN z.mes = 11 THEN 1 END) AS "11_Noviembre",
-        count(CASE WHEN z.mes = 12 THEN 1 END) AS "12_Diciembre"  
-    FROM (
-    SELECT  
-    y.c_usuario_vocal,  
-    y.n_unico ,  y.n_incidente ,
-    y.n_num_recurso, y.n_ano_recurso, y.nom_recurso ,y.f_programacion ,
-    y.anno ,
-    y.mes ,
-    (SELECT convert(DATE, b.f_firma)  
-    FROM ResolucionEditorFirma b
-    WHERE b.n_unico = y.n_unico AND
-    b.n_incidente = y.n_incidente AND
-    b.c_usuario    = y.c_usuario_vocal AND
-    b.l_firmado = 'S' AND
-    b.l_indPonente = 'S' AND b.l_activo = 'S' AND
-    b.f_firma = ( SELECT max(x.f_firma)
-    FROM ResolucionEditorFirma x
-    JOIN resolucion_editor r ON
-    r.n_unico = x.n_unico AND
-    r.n_incidente = x.n_incidente AND
-    r.f_descargo = x.f_descargo AND
-    IsNull(r.l_utilizado, 'N') <> 'A' AND
-    IsNull(r.l_ind_sumilla, 'N') <> 'N'
-    WHERE x.n_unico = b.n_unico AND
-    x.n_incidente = b.n_incidente AND
-    x.c_usuario = b.c_usuario AND
-    convert(DATE, x.f_firma)  >= convert(DATE, y.f_programacion)  AND
-    x.l_firmado = 'S' AND
-    x.l_indPonente = 'S' AND x.l_activo = 'S' )  ) AS firma    
-    FROM( SELECT cg.n_unico ,cg.n_incidente , cg.c_usuario_vocal,  
-    ie.n_exp_sala n_num_recurso,
-    ie.n_ano_sala n_ano_recurso ,
-    mim.x_desc_motivo_ingreso nom_recurso ,
-    cg.f_programacion ,
-    year(cg.f_programacion) anno,
-    month(cg.f_programacion)  mes,
-    cg.num_tipo_audiencia
-    FROM instancia_expediente ie (INDEX insta_expe_unic)  noholdlock
-    JOIN expediente e noholdlock
-    ON e.n_unico=ie.n_unico
-    AND e.n_incidente=ie.n_incidente
-    JOIN conformacion_grupo   cg noholdlock
-        ON ie.c_distrito = cg.c_distrito AND ie.c_provincia = cg.c_provincia
-                AND ie.c_instancia = cg.c_instancia AND ie.n_unico = cg.n_unico
-                AND ie.n_incidente = cg.n_incidente AND ie.f_ingreso = cg.f_ingreso
-                AND cg.l_ultimo = 'S' AND cg.l_ultimo_audiencia = 'S'
-                AND cg.l_reprogramado = 'N' AND cg.l_no_vista = 'N' AND cg.l_publicado = 'S'
-                AND cg.c_usuario_vocal IS NOT NULL    
-        JOIN motivo_ingreso_maestro mim noholdlock ON mim.c_motivo_ingreso = ie.c_motivo_ingreso      
-    WHERE ie.c_distrito = '50'
-            AND  ie.c_provincia = '01'
-            AND   ie.c_instancia =  '${n_sala}'  
-    and   cg.f_programacion BETWEEN ${fecha} 
-    AND ie.l_ultimo = 'S'
-    AND cg.num_tipo_audiencia <> 5
-    AND ISNULL(e.l_anulado,'N') = 'N'  ) y     )  z
-    WHERE z.f_programacion  <  isnull(z.firma , z.f_programacion)  
-    GROUP BY  z.c_usuario_vocal 
+    SELECT 
+    (CASE WHEN e.f_respuesta IS NULL THEN 'PENDIENTES'  
+    WHEN e.f_respuesta IS NOT NULL THEN 'ATENDIDOS' 
+    WHEN isnull(e.l_estado,'X') = 'A' THEN 'ANULADOS'
+    END ) AS  '00_x_estado',
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 1 THEN e.f_ingreso_acto END) AS "01_Enero",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 2 THEN e.f_ingreso_acto END) AS "02_Febrero",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 3 THEN e.f_ingreso_acto END) AS "03_Marzo",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 4 THEN e.f_ingreso_acto END) AS "04_Abril",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 5 THEN e.f_ingreso_acto END) AS "05_Mayo",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 6 THEN e.f_ingreso_acto END) AS "06_Junio",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 7 THEN e.f_ingreso_acto END) AS "07_Julio",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 8 THEN e.f_ingreso_acto END) AS "08_Agosto",	   	   
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 9 THEN e.f_ingreso_acto END) AS "09_Setiembre",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 10 THEN e.f_ingreso_acto END) AS "10_Octubre",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 11 THEN e.f_ingreso_acto END) AS "11_Noviembre",
+    count(CASE datepart(month,e.f_ingreso_acto) WHEN 12 THEN e.f_ingreso_acto END) AS "12_Diciembre"
+    FROM escrito e
+    JOIN instancia i
+    ON i.c_distrito = e.c_distrito AND
+    i.c_provincia = e.c_provincia AND
+    i.c_instancia = e.c_instancia
+    JOIN acto_procesal a
+    ON a.c_acto_procesal = e.c_acto_procesal
+    WHERE 
+    e.c_distrito = '50'
+    AND e.c_provincia = '01'
+    AND e.c_instancia =  '${n_sala}'
+    AND e.f_ingreso_acto BETWEEN ${fecha}
+    AND IsNull(e.l_estado, '') <> 'A'
+    AND a.l_parte = 'S'
+    AND a.l_redistribucion = 'N'
+    GROUP BY  (CASE WHEN e.f_respuesta IS NULL THEN 'PENDIENTES'  
+    WHEN e.f_respuesta IS NOT NULL THEN 'ATENDIDOS' 
+    WHEN isnull(e.l_estado,'X') = 'A' THEN 'ANULADOS'
+    END )  
 `;
 
 export const scripts = {
